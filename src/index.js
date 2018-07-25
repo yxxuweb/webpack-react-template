7237721488e7925d0393b6b68944efcd58784082
yxxu <yxxu_web@sina.com>
2018-07-25 19:15:26 +0800

    测试pre-commit功能

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import Loadable from 'react-loadable';
+console.log(123);
 
 const LoadableComponent = Loadable({
   loader: () => import('./test'),
